e671c189b33b092e25a0989b25b34b23af335f6b
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-03-21 18:56:05 -0300

    add proxy and redirect courses to api

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -9,14 +9,13 @@ import { Course } from './../model/course';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(5000),
       tap(courses => console.log(courses))
     )
   }
